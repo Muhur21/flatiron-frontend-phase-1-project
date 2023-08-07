0cc207b61b166880a95e57182a67e882c3b3d495
Mehary Gebremariam <mehary.aynealem3@gmail.com>
2023-08-07 02:18:50 -0500

    modify the main.js to add downkey event

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ const getClothCategories = async () => {
   const response = await fetch(ClothCategoriesDbJson);
   const clothes = await response.json();
   console.log(`clothes`, clothes);
-  console.table(clothes);
 
   const clothCategoryList = document.body.querySelector(
     "#cloth-categories-list"
@@ -20,17 +19,17 @@ const getClothCategories = async () => {
     clothCategoryList.appendChild(clothCategoryListItem);
 
     const clothCategoryImage = document.createElement("img");
-    clothCategoryImage.src = getClothCategories.picture;
+    clothCategoryImage.src = clothes.picture;
     clothCategoryImage.width = 300;
     clothCategoryListItem.appendChild(clothCategoryImage);
 
     const clothCategoryName = document.createElement("h4");
-    clothCategoryName.textContent = clothes?.name;
+    clothCategoryName.textContent = clothes.name;
 
     clothCategoryListItem.appendChild(clothCategoryName);
 
     const clothCategoryDescription = document.createElement("p");
-    clothCategoryDescription.textContent = clothes?.Description;
+    clothCategoryDescription.textContent = clothes.Description;
     clothCategoryListItem.appendChild(clothCategoryDescription);
   });
 };
@@ -42,7 +41,4 @@ const handleFormInputFocus = async () => {
 };
 
 searchItemsInput.addEventListener("focus", handleFormInputFocus());
-searchItemsInput.addEventListener("click", handleFormInputFocus());
-categoryFilter.addEventListener("click", handleFormInputFocus());
-
-
+categoryFilter.addEventListener("keydown", handleFormInputFocus());
